62ec83cf1b45d98b1b14fe1a14613dd5749c0ce7
Sevastos Matzouranis <sebamatz@gmail.com>
2025-12-16 12:24:34 +0200

    Refactor ColorOptions component for improved readability and structure
    - Enhanced the layout of the ColorOptions component by standardizing indentation and formatting for better code clarity.
    - Maintained existing functionality while improving the overall organization of the JSX structure, ensuring a more consistent coding style.

--- components/colors-selection/ColorOptions.tsx
+++ components/colors-selection/ColorOptions.tsx
@@ -6,15 +6,15 @@ import { useColorOptions } from "@/hooks/useColorOptions";
 export default function ColorOptions({ isDisabled }: { isDisabled: boolean }) {
   const { profilColors, handleProfilColors } = useColorOptions();
 
-
-  return <div className="space-y-4">
-  <Label className="text-base font-medium">Επιλογή χρώματος</Label>
-  <RadioGroup
-    value={profilColors}
-    onValueChange={handleProfilColors}
-    className="flex flex-row space-x-6"
-  >
-    {/* <div className="flex items-center space-x-2">
+  return (
+    <div className="space-y-4">
+      <Label className="text-base font-medium">Επιλογή χρώματος</Label>
+      <RadioGroup
+        value={profilColors}
+        onValueChange={handleProfilColors}
+        className="flex flex-row space-x-6"
+      >
+        {/* <div className="flex items-center space-x-2">
       <RadioGroupItem 
         value={profilColorsType.BLANK} 
         id="blank" 
@@ -24,36 +24,37 @@ export default function ColorOptions({ isDisabled }: { isDisabled: boolean }) {
         Άβαφο
       </Label>
     </div> */}
-    <div className="flex items-center space-x-2">
-      <RadioGroupItem 
-        value={profilColorsType.WHITE.colorType.toString()} 
-        id="white" 
-        disabled={isDisabled}
-      />
-      <Label htmlFor="white" className="text-sm font-normal">
-        Λευκό
-      </Label>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem
+            value={profilColorsType.WHITE.colorType.toString()}
+            id="white"
+            disabled={isDisabled}
+          />
+          <Label htmlFor="white" className="text-sm font-normal">
+            Λευκό
+          </Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem
+            value={profilColorsType.COLOR.colorType.toString()}
+            id="color"
+            disabled={isDisabled}
+          />
+          <Label htmlFor="color" className="text-sm font-normal">
+            Χρώμα
+          </Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem
+            value={profilColorsType.DUAL_COLOR.colorType.toString()}
+            id="color"
+            disabled={isDisabled}
+          />
+          <Label htmlFor="color" className="text-sm font-normal">
+            Διχρωμία
+          </Label>
+        </div>
+      </RadioGroup>
     </div>
-    <div className="flex items-center space-x-2">
-      <RadioGroupItem 
-        value={profilColorsType.COLOR.colorType.toString()} 
-        id="color" 
-        disabled={isDisabled}
-      />
-      <Label htmlFor="color" className="text-sm font-normal">
-        Χρώμα
-      </Label>
-    </div>
-    <div className="flex items-center space-x-2">
-      <RadioGroupItem 
-        value={profilColorsType.DUAL_COLOR.colorType.toString()} 
-        id="color" 
-        disabled={isDisabled}
-      />
-      <Label htmlFor="color" className="text-sm font-normal">
-        Δυχρωμία
-      </Label>
-    </div>
-  </RadioGroup>
-</div>
+  );
 }
